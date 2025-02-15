ab04d383fb7e29a81b0507be3b9f7ca66498e752
Muttaqi <mohammutt@sheridancollege.ca>
2025-02-15 01:24:51 -0500

    created if statements and elif statement for temp

--- UseOfSelections.py
+++ UseOfSelections.py
@@ -2,3 +2,12 @@ import random
 
 temper = random.randint(1,400)
 
+if temper > 100:
+    print("Temperature above boiling point")
+
+    if temper > 320:
+        print("Temperature above smoke point")
+
+elif temper < 100:
+    print("The temperature is not very high")
+
