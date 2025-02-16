a2b786e3c70374cbc2b858b69532d1d9d50189d8
Muttaqi <mohammutt@sheridancollege.ca>
2025-02-15 23:18:32 -0500

    created a print statement to show how many times the loop iterated

--- UseOfWhileLoop.py
+++ UseOfWhileLoop.py
@@ -1,9 +1,10 @@
 count = 0
 
 while True:
-    num = int(input("Enter a number >=0: "))
+    num = int(input("Enter a number >=0. If you type a negative number this loop will end: "))
 
     if num < 0:
         break
     count += 1
 
+print("The loop iterated",count, "times.")
