b22cd1076ba09c9ef357362c5a74aa224616b944
Muttaqi <mohammutt@sheridancollege.ca>
2025-02-16 17:53:34 -0500

    created a list and created avg function

---
+++ UseOfFunctions.py
@@ -0,0 +1,12 @@
+import math
+numbers = [1,3,5,6,9,11]
+def Avg(numbers):
+    average = sum(numbers) / len(numbers)
+    return average
+
+def sumSqDiff(numbers,average):
+
+
+A = Avg(numbers)
+print(A)
+
