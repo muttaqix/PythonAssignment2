9f49016fc0863fb5c30d4dce3b4652017de438a8
Muttaqi <mohammutt@sheridancollege.ca>
2025-02-15 23:16:28 -0500

    created while loop and used user input

--- UseOfForLoop.py
+++ UseOfForLoop.py
@@ -2,12 +2,9 @@ import math
 
 vector = [None,None,None]
 for i in range(3):
-    vector[i] = int(input("Enter a value > 0: "))
+    vector[i] = int(input("Enter a value: "))
     
 x,y,z = vector
 magnitude = math.sqrt(x**2 + y**2 + z**2)
 
 print(magnitude)
-
-
-
---
+++ UseOfWhileLoop.py
@@ -0,0 +1,9 @@
+count = 0
+
+while True:
+    num = int(input("Enter a number >=0: "))
+
+    if num < 0:
+        break
+    count += 1
+
