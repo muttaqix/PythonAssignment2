36bd38c046a3063db7fc7ff9da16b1c85c12b1e8
Muttaqi <mohammutt@sheridancollege.ca>
2025-02-16 16:46:03 -0500

    added variable StudentNumber and if statement if num = StudentNumber

--- UseOfWhileLoop.py
+++ UseOfWhileLoop.py
@@ -1,8 +1,14 @@
+StudentNumber = 991809457
+
 count = 0
 
 while True:
     num = int(input("Enter a number >=0. If you type a negative number this loop will end: "))
 
+    if num == StudentNumber:
+        print("Cutoff Point")
+        break
+    
     if num < 0:
         break
     count += 1
